0168c5405c94c8444823d721b517c9254e52088a
cornhundred <nickfloresfernandez@gmail.com>
2019-09-03 17:35:49 -0400

    updated run_cellranger_pipeline (which runs in container) to output argument

--- run_cellranger_pipeline.py
+++ run_cellranger_pipeline.py
@@ -17,32 +17,42 @@ import subprocess
 from common_utils.s3_utils import download_file, upload_file, download_folder, upload_folder
 from common_utils.job_utils import generate_working_dir, delete_working_dir
 
-# ###############################################################################
-# #
-# # parse arguments
-# #
-# ###############################################################################
-# # the first element is the script name, the second is the first argument
-# # the argument is specified by the cloudformation json
-# print('----------- CELLRANGER PIPELINE --------------\n\n')
-# print('parsing argument data\n------------------------------')
-# try:
-#   arg_string = sys.argv[1]
-#   arg_dict = json.loads(arg_string)
+###############################################################################
+#
+# parse arguments
+#
+###############################################################################
+# the first element is the script name, the second is the first argument
+# the argument is specified by the cloudformation json
+print('----------- CELLRANGER PIPELINE --------------\n\n')
+print('parsing argument data\n------------------------------')
+try:
+  arg_string = sys.argv[1]
+  arg_dict = json.loads(arg_string)
 
-#   inst_bucket = arg_dict['bucket']
-#   inst_fcs = arg_dict['inst_fcs']
+  print('------------------------------')
+  print('Able parse argument json')
+  print(arg_dict)
+  print('------------------------------')
 
-#   print('the bucket is: ' + str(inst_bucket))
-#   print('the folder has been hard wired')
-#   print('inst_fcs: ' + str(inst_fcs))
+  # inst_bucket = arg_dict['bucket']
+  # inst_fcs = arg_dict['inst_fcs']
 
-#   s3_folder = 'cytof_pipeline_data'
+  # print('the bucket is: ' + str(inst_bucket))
+  # print('the folder has been hard wired')
+  # print('inst_fcs: ' + str(inst_fcs))
 
-# except:
-#   print('------------------------------')
-#   print('unable to parse argument json')
-#   print('------------------------------')
+  # s3_folder = 'cytof_pipeline_data'
+
+except:
+  print('------------------------------')
+  print('Unable to parse argument json')
+  print('------------------------------')
+
+  try:
+    print(arg_string = sys.argv[1])
+  except:
+    pass
 
 
 
